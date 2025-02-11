ec614360ead9529f5461849a22ee7fd395a12c75
rodri <rodrigorodriguezmonge@gmail.com>
2025-02-11 14:24:14 +0100

    Estilos componente footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HomepageComponent } from './features/homepage/homepage.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/features/homepage/homepage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [FooterComponent],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
