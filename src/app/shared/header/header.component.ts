a55193d56116b45220691e1d1eff6302211ca462
rodri <rodrigorodriguezmonge@gmail.com>
2025-02-12 17:58:38 +0100

    Intervalo información

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  text: string = 'BIEVENIDO A EMPERX!';
+  texts: string[] = ['RETURNS AND EXCHANGES FREE OF SHIPPING CHARGES', 'ENVIO ESTANDAR GRATIS CON SUSCRIBCIÓN'];
+  currentIndex: number = 0;
+  fade: boolean = false;
 
+  ngOnInit() {
+    setInterval(() => {
+      this.fade = true;
+      setTimeout(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.texts.length;
+        this.text = this.texts[this.currentIndex];
+        this.fade = false;
+      }, 500); // Duración de la transición
+    }, 30000);
+  }
 }
