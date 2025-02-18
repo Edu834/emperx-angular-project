021a6103b2b57a6368b0a9d94a7379354f7214b7
rodri <rodrigorodriguezmonge@gmail.com>
2025-02-18 12:53:34 +0100

    Componente barra de busqueda

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from "../../shared/header/header.component";
+import { SearchComponent } from "../../shared/search/search.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, SearchComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 
--- src/app/features/how-works/how-works.component.ts
+++ src/app/features/how-works/how-works.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { SearchComponent } from "../../shared/search/search.component";
 
 @Component({
   selector: 'app-how-works',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SearchComponent],
   templateUrl: './how-works.component.html',
   styleUrl: './how-works.component.css'
 })
