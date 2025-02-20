0def70af8b4051b869a8736d2a84e01f3858771f
rodri <rodrigorodriguezmonge@gmail.com>
2025-02-20 12:29:43 +0100

    Filtros de genero

--- src/app/shared/search/filter-panel/filter-panel.component.ts
+++ src/app/shared/search/filter-panel/filter-panel.component.ts
@@ -1,32 +1,53 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-filter-panel',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filter-panel.component.html',
   styleUrl: './filter-panel.component.css'
 })
 export class FilterPanelComponent {
 
   products: any[] = [];
-  gender: string = ''; 
+  genderRoute: string = ''; 
   mostrarFiltros: boolean = false;
-  
+  isChecked = false;
+  category: string | undefined;
+  isCheckedMen: boolean | undefined;
+  isCheckedWomen: boolean | undefined;
 
   constructor(
     private route: ActivatedRoute, 
-    // private productsService: ProductsService 
+    private router: Router 
+    
   ) {}
-
+  
   ngOnInit(): void {
     
 
     this.route.paramMap.subscribe((params) => {
-      this.gender = params.get('gender') || '';
+      this.genderRoute = params.get('gender') || '';
       this.mostrarFiltros = !!params.get('category');
-
+      this.category = params.get('category') || '';
+      this.isCheckedMen = this.genderRoute === 'men';
+      this.isCheckedWomen = this.genderRoute === 'women';
     });
+  }
+  
+  // Método que cambia el estado de los botones
+  onCheckboxChange(gender: string) {
+    console.log(gender);
+    if (gender === 'men') {
+      this.isCheckedMen = true; // Activa el botón "Men"
+      this.isCheckedWomen = false; // Desactiva el botón "Women"
+      this.router.navigate(['/products/' + gender +'/'+ this.category]); // Cambia a la ruta para "Men"
+    } else if (gender === 'women') {
+      this.isCheckedWomen = true; // Activa el botón "Women"
+      this.isCheckedMen = false; // Desactiva el botón "Men"
+      this.router.navigate(['/products/' + gender +'/'+ this.category]); // Cambia a la ruta para "Women"
+    }
   }
 
   filterVisibility: { [key: string]: boolean } = {
@@ -40,35 +61,6 @@ export class FilterPanelComponent {
   toggleDropdown(filter: string) {
     this.filterVisibility[filter] = !this.filterVisibility[filter];
   }
-  // showGender = false;
-  // showSubcategory = false;
-  // showSize = false;
-  // showPriceRange = false;
-  // showBrand = false;
-  // showColor = false;
-  // priceValue = 100; // Valor inicial del rango de precio
 
-  // toggleDropdown(filter: string) {
-  //   switch (filter) {
-  //     case 'gender':
-  //       this.showGender = !this.showGender;
-  //       break;
-  //     case 'subcategory':
-  //       this.showSubcategory = !this.showSubcategory;
-  //       break;
-  //     case 'size':
-  //       this.showSize = !this.showSize;
-  //       break;
-  //     case 'priceRange':
-  //       this.showPriceRange = !this.showPriceRange;
-  //       break;
-  //     case 'brand':
-  //       this.showBrand = !this.showBrand;
-  //       break;
-  //     case 'color':
-  //       this.showColor = !this.showColor;
-  //       break;
-  //   }
-  // }
   
 }
