f7b7ebf15f5980f018a300df0da910aa2c9958fb
Eduardo <eduardoblazquez34@gmail.com>
2025-02-20 12:30:04 +0100

    Angular interface Articulo created

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -7,6 +7,47 @@ import { WomenProductsComponent } from "./women-products/women-products.componen
 import { MenProductsComponent } from "./men-products/men-products.component";
 import { ProductsService } from '../../core/service/products/products.service';
 
+interface Estado{
+  idEstado: number;
+  nombre: string;
+}
+interface Galeria{
+  
+}
+interface Categoria{
+  idcategoria: number;
+  nombre: string;
+  descripcion: string;
+}
+interface SubCategoria{
+  idSubcategoria: number;
+  nombre: string;
+  descripcion: string;
+  categoria: Categoria;
+}
+
+interface Producto{
+  idProducto: string;
+  descripcion: string;
+  marca: string;
+  nombre: string;
+  sexo: string;
+  galeria: Galeria;
+  subcategoria: SubCategoria;
+}
+
+interface Articulo{
+  idArticulo: string;
+  color: string;
+  descripcion: string;
+  nombre: string;
+  precio: number;
+  stock: number;
+  talla: string;
+  estados: Estado[];
+  producto: Producto;
+}
+
 @Component({
   selector: 'app-products',
   imports: [HeaderComponent, SearchComponent, FooterComponent, WomenProductsComponent, MenProductsComponent],
@@ -15,7 +56,8 @@ import { ProductsService } from '../../core/service/products/products.service';
 })
 export class ProductsComponent implements OnInit{
   showChild =  false;
-
+  
+  listaArticulos: Articulo[] = [];
   
   gender: string = ''; 
   mostrarCategoria: boolean = false;
@@ -27,6 +69,7 @@ export class ProductsComponent implements OnInit{
   
   idSubcategoria: number = 1;
   sexo: string = "H";
+  nombreCategoria: string = '';
   idCategoria: number = 1;
 
 
@@ -34,8 +77,13 @@ export class ProductsComponent implements OnInit{
     this.route.paramMap.subscribe((params) => {
       this.gender = params.get('gender') || '';
       this.mostrarCategoria = !!params.get('category');
-      
+      this.nombreCategoria = params.get('category') || '';
     });
+    if (this.gender === "women") {
+        this.sexo = "M";
+    }else{
+      this.sexo = "H";
+    }
     this.service.listArticulosPorSubCategoria(this.idSubcategoria).subscribe((data: any) => {
       console.log('Articulos por subcategoria:', data);
     });
@@ -44,7 +92,10 @@ export class ProductsComponent implements OnInit{
     });
     this.service.listArticulosPorSexo(this.sexo).subscribe((data: any) => {
       console.log('Articulos por sexo:', data);
+      this.listaArticulos = data;
+      console.log('LISTAAA DE ARTICULOSSS VARIABLEEE', this.listaArticulos);
     });
+
     this.service.listArticulosPorSexoAndCategoria(this.sexo, this.idCategoria).subscribe((data: any) => {
       console.log('Articulos por sexo y categoria:', data);
     });
