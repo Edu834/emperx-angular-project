f3c5f9c21d2b4a6f79ada47a79f52d0a29118992
Eduardo <eduardoblazquez34@gmail.com>
2025-02-23 19:46:16 +0100

    Mensagem de commit

--- src/app/shared/products-list/products-list.component.ts
+++ src/app/shared/products-list/products-list.component.ts
@@ -17,27 +17,7 @@ export class ProductsListComponent {
 
   @Input() filters: any = {};
 
-  // Aquí puedes manejar la lógica para mostrar productos según los filtros
-  ngOnChanges() {
-    console.log('Filtros recibidos:', this.filters);
-    console.log(this.filters.brand);
-    console.log(this.filters.color);
-    console.log(this.filters.price);
-
-    // Aquí puedes actualizar la lista de productos en función de los filtros
-  }
-
-  onFiltrosAplicados(filtros: any) {
-    this.filtros = filtros;
-    this.aplicarFiltros();
-  }
-
-  aplicarFiltros() {
-    // Aquí implementas la lógica para aplicar los filtros a los productos
-    console.log('Filtros aplicados:', this.filtros);
-  }
-
-    listaArticulos: Articulo[] = [];
+  listaArticulos: Articulo[] = [];
     categorias: Categoria[]=[];
     products: ProductView[] = [];
     gender: string = ''; 
@@ -54,43 +34,97 @@ export class ProductsListComponent {
     nombreCategoria: string = '';
     nombreSubcategoria: string = '';
     idCategoria: any = '';
+
+  // Aquí puedes manejar la lógica para mostrar productos según los filtros
+  ngOnChanges() {
+    console.log('Filtros recibidos:', this.filters);
+    console.log(this.filters.brand);
+    console.log(this.filters.color);
+    console.log(this.filters.price);
+    console.log(this.filters.size);
   
+    // Aquí puedes actualizar la lista de productos en función de los filtros
+    this.aplicarFiltros();
+  }
   
-    ngOnInit(): void {
-      this.route.paramMap.subscribe((params) => {
-        this.actualizarSexoYCategoria(params);
-        this.obtenerArticulos();
-      });
-    }
-    actualizarSexoYCategoria(params: any): void {
-      this.gender = params.get('gender') || '';
-      this.mostrarCategoria = !!params.get('category');
-      this.nombreCategoria = params.get('category') || '';
-      this.sexo = this.gender === "women" ? "M" : "H";
-    }
-    obtenerArticulos(): void {
-      this.service.listArticulos().subscribe({
-        next: (data: any) => {
-          this.listaArticulos = data;
-          this.cargarDatos(); // Filtrar los productos después de obtener los artículos
-        },
-        error: (error) => {
-          console.error('Error al cargar los artículos:', error);
-        }
-      });
-    }
+  onFiltrosAplicados(filtros: any) {
+    this.filtros = filtros;
+    this.aplicarFiltros();
+  }
+  
+  aplicarFiltros() {
+    console.log('Filtros aplicados:', this.filtros);
     
-    cargarDatos(): void{
-      if(this.nombreCategoria === '' && this.nombreSubcategoria === ''){
-        this.listaArticulos = this.listaArticulos.filter((articulo) => articulo.producto.sexo == this.sexo);
-        console.log('Articulos por sexo:', this.listaArticulos);
-        this.cargarCartasProductos();
-        console.log('Productos:', this.products);
-      }else if(this.nombreCategoria !== '' && this.nombreSubcategoria === ''){
+    // Filtrar la lista de artículos según los filtros aplicados
+    this.listaArticulos = this.listaArticulos.filter(articulo => {
+      let cumpleFiltros = true;
+  
+      if (this.filtros.brand) {
+        cumpleFiltros = cumpleFiltros && articulo.producto.marca === this.filtros.brand;
+      }
+      if (this.filtros.color) {
+        cumpleFiltros = cumpleFiltros && articulo.color === this.filtros.color;
+      }
+      if (this.filtros.price) {
+        cumpleFiltros = cumpleFiltros && articulo.precio <= this.filtros.price;
+      }
+      if (this.filtros.size) {
+        cumpleFiltros = cumpleFiltros && articulo.talla === this.filtros.size;
+      }
+  
+      // Añadir más condiciones de filtrado según sea necesario
+      return cumpleFiltros;
+    });
+    this.cargarCartasProductos();
+  }
+
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.actualizarSexoYCategoria(params);
+      this.obtenerArticulos();
+    });
+  }
+  actualizarSexoYCategoria(params: any): void {
+    this.gender = params.get('gender') || '';
+    this.mostrarCategoria = !!params.get('category');
+    this.nombreCategoria = params.get('category') || '';
+    this.sexo = this.gender === "women" ? "M" : "H";
+    this.nombreSubcategoria = params.get('subcategory') || '';
+    console.log('Sexo:', this.sexo);
+    console.log('Categoria:', this.nombreCategoria);
+    console.log('Subcategoria:', this.nombreSubcategoria);
+  }
+  obtenerArticulos(): void {
+    this.service.listArticulos().subscribe({
+      next: (data: any) => {
+        this.listaArticulos = data;
+        this.cargarDatos(); // Filtrar los productos después de obtener los artículos
+      },
+      error: (error) => {
+        console.error('Error al cargar los artículos:', error);
+      }
+    });
+  }
+  
+  cargarDatos(): void{
+    if(this.nombreCategoria === '' && this.nombreSubcategoria === ''){
+      this.listaArticulos = this.listaArticulos.filter((articulo) => articulo.producto.sexo == this.sexo);
+      console.log('Articulos por sexo:', this.listaArticulos);
+      this.cargarCartasProductos();
+      console.log('Productos:', this.products);
+    }else if(this.nombreCategoria !== '' && this.nombreSubcategoria === ''){
+      this.listaArticulos = this.listaArticulos.filter((articulo) => articulo.producto.subcategoria.categoria.nombre.toLocaleLowerCase() == this.nombreCategoria && articulo.producto.sexo == this.sexo);
+      console.log('Articulos por categoria:', this.listaArticulos);
+      this.cargarCartasProductos();
+      console.log('Productos:', this.products);
+    }else{
+      if(this.nombreSubcategoria === 'view-all'){
         this.listaArticulos = this.listaArticulos.filter((articulo) => articulo.producto.subcategoria.categoria.nombre.toLocaleLowerCase() == this.nombreCategoria && articulo.producto.sexo == this.sexo);
-        console.log('Articulos por categoria:', this.listaArticulos);
-        this.cargarCartasProductos();
-        console.log('Productos:', this.products);
+      console.log('Articulos por categoria:', this.listaArticulos);
+      this.cargarCartasProductos();
+      console.log('Productos:', this.products);
       }else{
         this.listaArticulos = this.listaArticulos.filter((articulo) => articulo.producto.subcategoria.nombre.toLocaleLowerCase() == this.nombreSubcategoria && articulo.producto.sexo == this.sexo);
         console.log('Articulos por subcategoria:', this.listaArticulos);
@@ -98,60 +132,39 @@ export class ProductsListComponent {
         console.log('Productos:', this.products);
       }
     }
+  }
 
-    cargarCartasProductos(): void {
-      this.products = [];
-      let i = 0;
-      this.listaArticulos.forEach(e => {
-        let product = this.products.find(product => product.producto.idProducto === e.producto.idProducto);
-        if (product === undefined) {
-          i++;
-          let tallas: string[] = [e.talla];
-          let colores: string[] = [e.color];
-          let articulos: string[] = [e.idArticulo];
-          this.products.push({
-            id: i,
-            producto: e.producto,
-            name: e.producto.nombre,
-            price: e.precio,
-            imageUrl: 'https://via.placeholder.com/150',
-            stock: 1,
-            color: colores,
-            size: tallas,
-            articulos: articulos
-          });
-        } else {
-          product.stock += 1;
-          if (!product.color.includes(e.color)) {
-            product.color.push(e.color);
-          }
-          if (!product.size.includes(e.talla)) {
-            product.size.push(e.talla);
-          }
-          product.articulos.push(e.idArticulo);
+  cargarCartasProductos(): void {
+    this.products = [];
+    let i = 0;
+    this.listaArticulos.forEach(e => {
+      let product = this.products.find(product => product.producto.idProducto === e.producto.idProducto);
+      if (product === undefined) {
+        i++;
+        let tallas: string[] = [e.talla];
+        let colores: string[] = [e.color];
+        let articulos: string[] = [e.idArticulo];
+        this.products.push({
+          id: i,
+          producto: e.producto,
+          name: e.producto.nombre,
+          price: e.precio,
+          imageUrl: 'https://via.placeholder.com/150',
+          stock: 1,
+          color: colores,
+          size: tallas,
+          articulos: articulos
+        });
+      } else {
+        product.stock += 1;
+        if (!product.color.includes(e.color)) {
+          product.color.push(e.color);
         }
-      });
-    }
-  // products: ProductView[] = [
-  //   { id: 1, name: 'Producto 1', price: 100, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 2, name: 'Producto 2', price: 200, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 3, name: 'Producto 3', price: 300, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 4, name: 'Producto 4', price: 400, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 5, name: 'Producto 5', price: 500, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 6, name: 'Producto 6', price: 600, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 7, name: 'Producto 7', price: 700, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 8, name: 'Producto 8', price: 800, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 9, name: 'Producto 9', price: 900, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 10, name: 'Producto 10', price: 1000, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 11, name: 'Producto 11', price: 1100, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 12, name: 'Producto 12', price: 1200, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 13, name: 'Producto 13', price: 1300, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 14, name: 'Producto 14', price: 1400, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 15, name: 'Producto 15', price: 1500, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 16, name: 'Producto 16', price: 1600, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 17, name: 'Producto 17', price: 1700, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 18, name: 'Producto 18', price: 1800, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 19, name: 'Producto 19', price: 1900, imageUrl: 'https://via.placeholder.com/150' },
-  //   { id: 20, name: 'Producto 20', price: 2000, imageUrl: 'https://via.placeholder.com/150' }
-  // ];
+        if (!product.size.includes(e.talla)) {
+          product.size.push(e.talla);
+        }
+        product.articulos.push(e.idArticulo);
+      }
+    });
+  }
 }
