ed24b6498e1d78b28f01dde53b88b5640b279a9e
rodri <rodrigorodriguezmonge@gmail.com>
2025-03-05 14:03:00 +0100

    women y men poster

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -3,10 +3,11 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { SearchComponent } from "../../shared/search/search.component";
 import { CarrouselComponent } from "./carrousel/carrousel.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [FooterComponent, HeaderComponent, SearchComponent, CarrouselComponent],
+  imports: [FooterComponent, HeaderComponent, SearchComponent, CarrouselComponent, RouterLink],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
